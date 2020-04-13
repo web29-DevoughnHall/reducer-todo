147a9015d0cbdd3ccd7690ce549c101a9b855e22
1devhall <devoughnh@gmail.com>
2020-04-13 17:49:54 -0400

    mvp

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,29 @@
+export const initialState = [{
+	todo: '',
+	completed: false,
+	id: new Date()
+}]
+
+
+export function reducer(state, action) {
+	switch(action.type) {
+
+		case "ADD_TODO":
+			return [...state, action.payload]
+
+		case "COMPLETED_TODO":
+			return state.map((item) => {
+					return item.id === action.payload ?
+				{...item, completed: !item.completed} : item
+			}
+		)
+
+		case "CLEAR_COMPLETED_TODO" : 
+			return state.filter((item) => !item.completed)
+
+		default:
+			return state
+
+	}
+
+};
